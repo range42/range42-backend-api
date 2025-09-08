0962af606cb3c31c577ec91496e7581bbb2f326d
Benjamin Collas <129123125+hyde-repo@users.noreply.github.com>
2025-09-08 23:36:01 +0200

    feat: /v0/debug/ping routes - first test

---
+++ app/routes/v0/debug/ping.py
@@ -0,0 +1,104 @@
+from typing import Any
+
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
+
+from app.runner import  run_playbook_core # , extract_action_results
+from app.schemas.debug.ping import Request_DebugPing
+
+from pathlib import Path
+import os
+
+router = APIRouter()
+
+# PROJECT_ROOT = Path(__file__).resolve().parents[4]
+PROJECT_ROOT = Path(os.getenv("PROJECT_ROOT_DIR")).resolve()
+PLAYBOOK_SRC = PROJECT_ROOT / "playbooks" / "ping.yml"
+INVENTORY_SRC = PROJECT_ROOT / "inventory" / "hosts.yml"
+
+
+debug = 0
+
+@router.post(
+    path="/ping",
+    summary="Run Ansible ping utility",
+    description="This endpoint runs the Ansible ping module to check connectivity with target hosts.",
+    tags=["utils - debug"],
+)
+
+def debug_ping(req: Request_DebugPing):
+
+
+
+    if debug ==1:
+        print("::  REQUEST ::", req.dict())
+        print(f":: PROJECT_ROOT  :: {PROJECT_ROOT} ")
+        print(f":: PLAYBOOK_SRC  :: {PLAYBOOK_SRC} ")
+        print(f":: INVENTORY_SRC :: {INVENTORY_SRC} ")
+
+    if not PLAYBOOK_SRC.exists():
+        raise HTTPException(status_code=400, detail=f":: MISSING PLAYBOOK : {PLAYBOOK_SRC}")
+    if not INVENTORY_SRC.exists():
+        raise HTTPException(status_code=400, detail=f":: MISSING INVENTORY : {INVENTORY_SRC}")
+
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+    extravars = request_checks(req)
+
+    ####
+
+    rc, events, log_plain, log_ansi = run_playbook_core(
+        PLAYBOOK_SRC,
+        INVENTORY_SRC,
+        limit=req.hosts,
+        extravars=extravars,
+    )
+
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+    payload = reply_processing(log_plain, rc)
+
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+    if rc == 0:
+        status = 200
+    else:
+        status = 500
+
+    return JSONResponse(payload, status_code=status)
+
+
+def reply_processing(log_plain: str,
+                     rc
+                     ) -> dict[str, list[str] | Any]:
+
+    """ reply post-processing - for ansible raw output """
+
+    payload = {"rc": rc, "log_multiline": log_plain.splitlines()}
+
+    return payload
+
+
+def request_checks(req: Request_DebugPing) -> dict[Any, Any]:
+    """ requet checks """
+
+    extravars = {}
+
+    if req.proxmox_node:
+        extravars["proxmox_node"] = req.proxmox_node
+
+    # nothing :
+    if not extravars:
+        extravars = None
+
+    if debug == 1:
+        print(f", extra vars : {extravars}")
+
+    return extravars
+
