67293e36382f38341a2d7c3143c79f80e148a285
Benjamin Collas <129123125+hyde-repo@users.noreply.github.com>
2025-09-08 23:54:22 +0200

    chore: first start script

--- start.sh
+++ start.sh
@@ -3,8 +3,21 @@
 #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
 
 PROJECT_ROOT="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
-APP_DIR="$PROJECT_ROOT/app"
+export PROJECT_ROOT_DIR="$PROJECT_ROOT"
+APP_DIR="$PROJECT_ROOT_DIR/app"
 APP_MODULE="app.main:app"
+
+
+export API_BACKEND_PUBLIC_PLAYBOOKS_DIR="$HOME/_products.git-hyde-repo/range42-infrastructures-installers/"
+export API_BACKEND_WWWAPP_PLAYBOOKS_DIR="$PROJECT_ROOT_DIR/"
+export API_BACKEND_INVENTORY_DIR="$PROJECT_ROOT_DIR/inventory/"
+export API_BACKEND_VAULT_FILE="$HOME/_products.git-hyde-repo/range42-ansible_roles-private-devkit//secrets/px-testing.cr42_tailscale.yaml"
+#
+# vault pwd
+#
+export VAULT_PASSWORD_FILE="/tmp/vault/vault_pass.txt"
+#export VAULT_PASSWORD="redacted.
+
 
 HOST="0.0.0.0"
 PORT="8000"
@@ -13,21 +26,15 @@ WORKERS=1
 #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
 
 # move to project + export
-cd "$PROJECT_ROOT"
+cd "$PROJECT_ROOT_DIR" || exit
 export PYTHONPATH="$PROJECT_ROOT:${PYTHONPATH:-}"
 
-#
-# vault pwd
-#
-export VAULT_PASSWORD_FILE="/tmp/vault/vault_pass.txt"
-#export VAULT_PASSWORD="redacted.
 
 #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
 
-echo ":: start :: $APP_MODULE - $PROJECT_ROOT"
+echo ":: start :: $APP_MODULE - $PROJECT_ROOT_DIR"
 exec uvicorn "$APP_MODULE" \
   --host "$HOST" \
   --port "$PORT" \
   --workers "$WORKERS" \
-  --log-level info \
-  --reload
+  --log-level info  --reload
