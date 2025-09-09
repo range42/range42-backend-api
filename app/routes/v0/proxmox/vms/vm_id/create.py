61b35854d67b2036113b624c99f6b6115f5b9dec
Benjamin Collas <129123125+hyde-repo@users.noreply.github.com>
2025-09-09 23:39:33 +0200

    feat: (#3) - add create vm route and schema

---
+++ app/routes/v0/proxmox/vms/vm_id/create.py
@@ -0,0 +1,178 @@
+from typing import Any
+import logging
+from fastapi import APIRouter, HTTPException, Body
+
+from fastapi.responses import JSONResponse
+
+from app.schemas.proxmox.vm_id.create import Request_ProxmoxVmsVMID_Create
+from app.schemas.proxmox.vm_id.create import Reply_ProxmoxVmsVMID_Create
+
+from app.runner import  run_playbook_core # , extract_action_results
+from app.json_extract import extract_action_results
+from app import utils
+from pathlib import Path
+
+import os
+
+#
+# ISSUE - #3
+#
+
+debug = 0
+
+router = APIRouter()
+logger = logging.getLogger(__name__)
+
+PROJECT_ROOT = Path(os.getenv("PROJECT_ROOT_DIR")).resolve()
+INVENTORY_NAME = "hosts"
+PLAYBOOK_SRC = PROJECT_ROOT / "playbooks" / "generic.yml"
+
+#
+# => /api/proxmox/vms/vmd_id/create - POST
+#
+
+@router.post(
+    path="/create",
+    summary="Create a specific VM",
+    description="This endpoint create the target virtual machine (VM).",
+    tags=["proxmox - vm create"],
+    #
+    response_model=Reply_ProxmoxVmsVMID_Create,
+    response_description="Delete result",
+)
+
+def proxmox_vms_vm_id_pause(req: Request_ProxmoxVmsVMID_Create):
+    """ This endpoint clone the target virtual machine (VM)."""
+
+    if not PLAYBOOK_SRC.exists():
+        err = f":: err - MISSING PLAYBOOK : {PLAYBOOK_SRC}"
+        logger.error(err)
+        raise HTTPException(status_code=400, detail=err)
+
+    checked_playbook_filepath  = PLAYBOOK_SRC
+    checked_inventory_filepath = utils.resolve_inventory(INVENTORY_NAME)
+
+    if debug ==1:
+        print("")
+        print("::  REQUEST ::", req.model_dump())
+        print(f":: checked_inventory_filepath :: {checked_inventory_filepath} ")
+        print(f":: checked_playbook_filepath  :: {checked_playbook_filepath} ")
+        print("")
+
+
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+    extravars = request_checks(req)
+
+    ####
+
+    rc, events, log_plain, log_ansi = run_playbook_core(
+        checked_playbook_filepath,
+        checked_inventory_filepath,
+        # limit=req.hosts,
+        limit=extravars["hosts"],
+        extravars=extravars,
+    )
+
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+    payload = reply_processing(events, extravars, log_plain, rc, req)
+
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+    if rc == 0:
+        status = 200
+    else:
+        status = 500
+
+    return JSONResponse(payload, status_code=status)
+
+
+def reply_processing(events: list[dict] | list[Any],
+                     extravars: dict[Any, Any],
+                     log_plain: str,
+                     rc,
+                     req: Request_ProxmoxVmsVMID_Create) -> dict[str, list | Any]:
+
+    """ reply post-processing - json or ansible raw output """
+
+    if req.as_json:
+
+        ##### OUTPUT TYPE - as_json=True
+        #####
+
+        action = extravars["proxmox_vm_action"]
+        result = extract_action_results(events, action)
+
+        payload = {
+            "rc": rc,
+            "result": result
+            # "action": action,
+        }  # raw
+
+        # payload = {"rc": rc, "action": action, "result": events}
+    else:
+        ####
+        #### OUTPUT AS TEXT - as_json=False
+        ####
+
+        # payload = {"rc": rc, "log_plain": log_plain, "log_multiline": log_plain.splitlines()}
+        payload = {"rc": rc, "log_multiline": log_plain.splitlines()}
+    return payload
+
+
+def request_checks(req: Request_ProxmoxVmsVMID_Create) -> dict[Any, Any]:
+    """ request checks """
+
+    extravars = {}
+
+    extravars["proxmox_vm_action"] = "vm_create"
+
+    if req.proxmox_node:
+        extravars["proxmox_node"] = req.proxmox_node
+
+    if req.vm_id is not None:
+        extravars["vm_id"] = req.vm_id
+
+    if req.vm_name is not None:
+        extravars["vm_name"] = req.vm_name
+
+    if req.vm_cpu is not None:
+        extravars["vm_cpu"] = req.vm_cpu
+
+    if req.vm_cores is not None:
+        extravars["vm_cores"] = req.vm_cores
+
+    if req.vm_sockets is not None:
+        extravars["vm_sockets"] = req.vm_sockets
+
+    if req.vm_memory is not None:
+        extravars["vm_memory"] = req.vm_memory
+
+    if req.vm_disk_size is not None:
+        extravars["vm_disk_size"] = req.vm_disk_size
+
+    if req.vm_iso is not None:
+        extravars["vm_iso"] = req.vm_iso
+
+
+    # if req.vm_description is not None:
+    #     extravars["vm_description"] = req.vm_description
+
+
+    # nothing :
+    if not extravars:
+        extravars = None
+
+    if debug == 1:
+        print(f", extra vars : {extravars}")
+
+    extravars["hosts"] = "proxmox"
+    return extravars
+
---
+++ app/schemas/proxmox/vm_id/create.py
@@ -0,0 +1,160 @@
+
+from typing import Literal
+from pydantic import BaseModel, Field
+
+#
+# ISSUE - #3
+#
+
+class Request_ProxmoxVmsVMID_Create(BaseModel):
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "proxmox_node": "px-testing",
+                "vm_id": 1111,
+                "vm_name": "vm_with_local_iso",
+                "vm_cpu": "host",
+                "vm_cores": 2,
+                "vm_sockets": 1,
+                "vm_memory": 2042,
+                "vm_disk_size": 42,
+                "vm_iso": "local:iso/ubuntu-24.04.2-live-server-amd64.iso"
+            }
+        }
+    }
+
+class Request_ProxmoxVmsVMID_Create(BaseModel):
+
+    proxmox_node: str = Field(
+        ...,
+        # default= "px-testing",
+        description = "Proxmox node name",
+        pattern=r"^[A-Za-z0-9-]*$"
+    )
+
+    vm_id: str = Field(
+        ...,
+        # default="4000",
+        description="Virtual machine id",
+        pattern=r"^[0-9]+$"
+    )
+
+    vm_name: str  = Field(
+        ...,
+        # default="new-vm",
+        description="Virtual machine meta name",
+        pattern = r"^[A-Za-z0-9-]*$"
+    )
+
+    vm_cpu: str = Field(
+        ...,
+        # default= "host",
+        description='CPU type/model - host)',
+        pattern=r"^[A-Za-z0-9._-]+$"
+    )
+
+    vm_cores: int = Field(
+        ...,
+        # default=1,
+        ge=1,
+        description="Number of cores per socket"
+    )
+
+    vm_sockets: int = Field(
+        ...,
+        # default=1,
+        ge=1,
+        description="Number of CPU sockets"
+    )
+
+    vm_memory: int = Field(
+        ...,
+        # default=1024,
+        ge=128,
+        description="Memory in MiB"
+    )
+
+    vm_disk_size: int | None = Field(
+        default=None,
+        ge=1,
+        description="Disk size in GiB - optional"
+    )
+
+    vm_iso: str | None = Field(
+        default=None,
+        description="ISO volume path like 'local:iso/xxx.iso' - optional",
+        pattern=r"^[A-Za-z0-9._-]+:iso/.+\.iso$"
+    )
+
+    as_json: bool = Field(
+        default=True,
+        description="If true : JSON output else : raw output"
+    )
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "proxmox_node": "px-testing",
+                "vm_id": "1111",
+                "vm_name": "new-vm",
+                "vm_cpu": "host",
+                "vm_cores": 2,
+                "vm_sockets": 1,
+                "vm_memory": 2042,
+                "vm_disk_size": 42,
+                "vm_iso": "local:iso/ubuntu-24.04.2-live-server-amd64.iso"
+            }
+        }
+    }
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+class Reply_ProxmoxVmsVMID_CreateItem(BaseModel):
+
+    action: Literal["vm_create"]
+    source: Literal["proxmox"]
+    proxmox_node: str
+    vm_id: int = Field(..., ge=1)
+    vm_name: str
+    vm_cpu: str
+    vm_cores: int = Field(..., ge=1)
+    vm_sockets: int = Field(..., ge=1)
+    vm_memory: int = Field(..., ge=1)
+    vm_net0: str
+    vm_scsi0: str
+    raw_data: str = Field(..., description="Raw string returned by Proxmox")
+
+
+class Reply_ProxmoxVmsVMID_Create(BaseModel):
+
+    rc: int = Field(0, description="RETURN code (0 = OK)")
+    result: list[Reply_ProxmoxVmsVMID_CreateItem]
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "rc": 0,
+                "result": [
+                    {
+                        "action": "vm_create",
+                        "proxmox_node": "px-testing",
+                        "raw_data": "UPID:px-testing:00281144:16855865:68C04C13:qmcreate:9998:API_master@pam!API_master:",
+                        "source": "proxmox",
+                        "vm_cores": 2,
+                        "vm_cpu": "host",
+                        "vm_id": 9998,
+                        "vm_memory": 2042,
+                        "vm_name": "vm-with-local-iso-2",
+                        "vm_net0": "virtio,bridge=vmbr0",
+                        "vm_scsi0": "local-lvm:42,format=raw",
+                        "vm_sockets": 1
+                    }
+                ]
+            }
+        }
+    }
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
