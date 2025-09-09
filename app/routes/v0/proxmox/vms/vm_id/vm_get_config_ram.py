321676654ea7281de9b059db6684ff6205ceaf53
Benjamin Collas <129123125+hyde-repo@users.noreply.github.com>
2025-09-10 00:35:03 +0200

    feat: (#5) - add route and schema for vm get config - ram configuration

---
+++ app/routes/v0/proxmox/vms/vm_id/vm_get_config_ram.py
@@ -0,0 +1,158 @@
+from typing import Any
+import logging
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
+
+from app.schemas.proxmox.vm_id.vm_get_config_ram import Request_ProxmoxVmsVMID_VmGetConfigRam
+from app.schemas.proxmox.vm_id.vm_get_config_ram import Reply_ProxmoxVmsVMID_VmGetConfigRam
+
+from app.runner import  run_playbook_core # , extract_action_results
+from app.json_extract import extract_action_results
+from app import utils
+from pathlib import Path
+import os
+
+#
+# ISSUE - #5
+#
+
+debug = 0
+
+router = APIRouter()
+logger = logging.getLogger(__name__)
+
+# PROJECT_ROOT = Path(__file__).resolve().parents[6]
+PROJECT_ROOT = Path(os.getenv("PROJECT_ROOT_DIR")).resolve()
+INVENTORY_NAME = "hosts"
+PLAYBOOK_SRC = PROJECT_ROOT / "playbooks" / "generic.yml"
+
+# INVENTORY_SRC = PROJECT_ROOT / "inventory" / "hosts.yml"
+
+# @router.post("/{vm_id}/start")
+# def proxmox_vms_vm_id_start(
+#     vm_id: int,
+#     req: Request_ProxmoxVmsVMID_StartStopPauseResume,
+# ):
+
+#
+# => /api/proxmox/vms/vmd_id/pause
+#
+@router.post(
+    path="/vm_get_config_ram",
+    summary="Get configuration of a specific VM",
+    description="This endpoint get the target virtual machine (VM) configuration details.",
+    tags=["proxmox - vm lifecycle"],
+    #
+    response_model=Reply_ProxmoxVmsVMID_VmGetConfigRam,
+    response_description="Start result",
+)
+
+def proxmox_vms_vm_id_pause(req: Request_ProxmoxVmsVMID_VmGetConfigRam):
+    """ This endpoint pauses the target virtual machine (VM)."""
+
+    if not PLAYBOOK_SRC.exists():
+        err = f":: err - MISSING PLAYBOOK : {PLAYBOOK_SRC}"
+        logging.error(err)
+        raise HTTPException(status_code=400, detail=err)
+
+    checked_playbook_filepath  = PLAYBOOK_SRC
+    checked_inventory_filepath = utils.resolve_inventory(INVENTORY_NAME)
+
+
+    if debug ==1:
+        print("")
+        print("::  REQUEST ::", req.dict())
+        print(f":: checked_inventory_filepath :: {checked_inventory_filepath} ")
+        print(f":: checked_playbook_filepath  :: {checked_playbook_filepath} ")
+        print("")
+
+
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+    extravars = request_checks(req)
+
+    ####
+
+    rc, events, log_plain, log_ansi = run_playbook_core(
+        checked_playbook_filepath,
+        checked_inventory_filepath,
+        # limit=req.hosts,
+        limit=extravars["hosts"],
+        extravars=extravars,
+    )
+
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+    payload = reply_processing(events, extravars, log_plain, rc, req)
+
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+    if rc == 0:
+        status = 200
+    else:
+        status = 500
+
+    return JSONResponse(payload, status_code=status)
+
+
+def reply_processing(events: list[dict] | list[Any],
+                     extravars: dict[Any, Any],
+                     log_plain: str,
+                     rc,
+                     req: Request_ProxmoxVmsVMID_VmGetConfigRam) -> dict[str, list | Any]:
+
+    """ reply post-processing - json or ansible raw output """
+
+    if req.as_json:
+
+        ##### OUTPUT TYPE - as_json=True
+        #####
+
+        action = extravars["proxmox_vm_action"]
+        result = extract_action_results(events, action)
+
+        payload = {
+            "rc": rc,
+            "result": result
+            # "action": action,
+        }  # raw
+
+        # payload = {"rc": rc, "action": action, "result": events}
+    else:
+        ####
+        #### OUTPUT AS TEXT - as_json=False
+        ####
+
+        # payload = {"rc": rc, "log_plain": log_plain, "log_multiline": log_plain.splitlines()}
+        payload = {"rc": rc, "log_multiline": log_plain.splitlines()}
+    return payload
+
+
+def request_checks(req: Request_ProxmoxVmsVMID_VmGetConfigRam) -> dict[Any, Any]:
+    """ request checks """
+
+    extravars = {}
+    extravars["proxmox_vm_action"] = "vm_get_config_ram"
+
+    if req.vm_id is not None:
+        extravars["vm_id"] = req.vm_id
+
+    if req.proxmox_node:
+        extravars["proxmox_node"] = req.proxmox_node
+
+    # nothing :
+    if not extravars:
+        extravars = None
+
+    if debug == 1:
+        print(f", extra vars : {extravars}")
+
+    extravars["hosts"] = "proxmox"
+    return extravars
+
---
+++ app/schemas/proxmox/vm_id/vm_get_config_ram.py
@@ -0,0 +1,77 @@
+
+from typing import Literal
+from pydantic import BaseModel, Field
+
+#
+# ISSUE - #5
+#
+
+class Request_ProxmoxVmsVMID_VmGetConfigRam(BaseModel):
+
+    proxmox_node: str = Field(
+        ...,
+        # default= "px-testing",
+        description = "Proxmox node name",
+        pattern=r"^[A-Za-z0-9-]*$"
+    )
+
+    vm_id: str = Field(
+        ...,
+        # default="4000",
+        description="Virtual machine id",
+        pattern=r"^[0-9]+$"
+    )
+
+    as_json: bool = Field(
+        default=True,
+        description="If true : JSON output else : raw output"
+    )
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "proxmox_node": "px-testing",
+                "vm_id": "1111",
+                "as_json": True,
+            }
+        }
+    }
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+class Reply_ProxmoxVmsVMID_VmGetConfigRamItem(BaseModel):
+
+    action: Literal["vm_get_config"]
+    source: Literal["proxmox"]
+    proxmox_node: str
+    vm_id: int = Field(..., ge=1)
+    vm_name: str
+    raw_data: str = Field(..., description="Raw string returned by proxmox")
+
+
+class Reply_ProxmoxVmsVMID_VmGetConfigRam(BaseModel):
+
+    rc: int = Field(0, description="RETURN code (0 = OK)")
+    result: list[Reply_ProxmoxVmsVMID_VmGetConfigRamItem]
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "rc": 0,
+                "result": [
+                    {
+                        "action": "vm_get_config_ram",
+                        "proxmox_node": "px-testing",
+                        "source": "proxmox",
+                        "vm_id": "1000",
+                        "vm_ram_allocated": "8192"
+                    }
+                ]
+            }
+        }
+    }
+
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
