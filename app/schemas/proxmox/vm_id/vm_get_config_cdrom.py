fb5dba97e50ea58bbe8579a7d9563590e5d4cf9e
Benjamin Collas <129123125+hyde-repo@users.noreply.github.com>
2025-09-10 00:34:10 +0200

    feat: (#5) - add route and schema for vm get config - cdrom configuration

---
+++ app/schemas/proxmox/vm_id/vm_get_config_cdrom.py
@@ -0,0 +1,80 @@
+
+from typing import Literal
+from pydantic import BaseModel, Field
+
+#
+# ISSUE - #5
+#
+
+class Request_ProxmoxVmsVMID_VmGetConfigCdrom(BaseModel):
+
+    proxmox_node: str = Field(
+        ...,
+        # default= "px-testing",
+        description = "Proxmox node name",
+        pattern=r"^[A-Za-z0-9-]*$"
+    )
+
+    vm_id: str = Field(
+        ...,
+        # default="4000",
+        description="Virtual machine id",
+        pattern=r"^[0-9]+$"
+    )
+
+    as_json: bool = Field(
+        default=True,
+        description="If true : JSON output else : raw output"
+    )
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "proxmox_node": "px-testing",
+                "vm_id": "1111",
+                "as_json": True,
+            }
+        }
+    }
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+class Reply_ProxmoxVmsVMID_VmGetConfigCdromItem(BaseModel):
+
+    action: Literal["vm_get_config"]
+    source: Literal["proxmox"]
+    proxmox_node: str
+    vm_id: int = Field(..., ge=1)
+    vm_name: str
+    raw_data: str = Field(..., description="Raw string returned by proxmox")
+
+
+class Reply_ProxmoxVmsVMID_VmGetConfigCdrom(BaseModel):
+
+    rc: int = Field(0, description="RETURN code (0 = OK)")
+    result: list[Reply_ProxmoxVmsVMID_VmGetConfigCdromItem]
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "rc": 0,
+                "result": [
+                    {
+                        "action": "vm_get_config_cdrom",
+                        "proxmox_node": "px-testing",
+                        "source": "proxmox",
+                        "vm_cdrom_device": "ide2",
+                        "vm_cdrom_iso": "local:1000/vm-1000-cloudinit.qcow2",
+                        "vm_cdrom_media": "cdrom",
+                        "vm_cdrom_size": "4M",
+                        "vm_id": "1000"
+                    }
+                ]
+            }
+        }
+    }
+
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
