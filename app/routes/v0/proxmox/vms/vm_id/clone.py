a57dd5323df17de40fc316c64ce21a5dd5d2a4d0
Benjamin Collas <129123125+hyde-repo@users.noreply.github.com>
2025-09-09 23:37:09 +0200

    feat: (#3) - add clone route and schema

---
+++ app/routes/v0/proxmox/vms/vm_id/clone.py
@@ -0,0 +1,159 @@
+from typing import Any
+import logging
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
+
+from app.schemas.proxmox.vm_id.clone import Request_ProxmoxVmsVMID_Clone
+from app.schemas.proxmox.vm_id.clone import Reply_ProxmoxVmsVMID_Clone
+
+from app.runner import  run_playbook_core # , extract_action_results
+from app.json_extract import extract_action_results
+from app import utils
+from pathlib import Path
+import os
+
+#
+# ISSUE - #3
+#
+
+debug = 0
+
+router = APIRouter()
+logger = logging.getLogger(__name__)
+
+PROJECT_ROOT = Path(os.getenv("PROJECT_ROOT_DIR")).resolve()
+INVENTORY_NAME = "hosts"
+PLAYBOOK_SRC = PROJECT_ROOT / "playbooks" / "generic.yml"
+
+#
+# => /api/proxmox/vms/vmd_id/clone - POST
+#
+@router.post(
+    path="/clone",
+    summary="Clone a specific VM",
+    description="This endpoint clone the target virtual machine (VM).",
+    tags=["proxmox - vm clone"],
+    #
+    response_model=Reply_ProxmoxVmsVMID_Clone,
+    response_description="Delete result",
+)
+
+def proxmox_vms_vm_id_pause(req: Request_ProxmoxVmsVMID_Clone):
+    """ This endpoint clone the target virtual machine (VM)."""
+
+    if not PLAYBOOK_SRC.exists():
+        err = f":: err - MISSING PLAYBOOK : {PLAYBOOK_SRC}"
+        logging.error(err)
+        raise HTTPException(status_code=400, detail=err)
+
+    checked_playbook_filepath  = PLAYBOOK_SRC
+    checked_inventory_filepath = utils.resolve_inventory(INVENTORY_NAME)
+
+    if debug ==1:
+        print("")
+        print("::  REQUEST ::", req.dict())
+        print(f":: checked_inventory_filepath :: {checked_inventory_filepath} ")
+        print(f":: checked_playbook_filepath  :: {checked_playbook_filepath} ")
+        print("")
+
+
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+    extravars = request_checks(req)
+
+    ####
+
+    rc, events, log_plain, log_ansi = run_playbook_core(
+        checked_playbook_filepath,
+        checked_inventory_filepath,
+        # limit=req.hosts,
+        limit=extravars["hosts"],
+        extravars=extravars,
+    )
+
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+    payload = reply_processing(events, extravars, log_plain, rc, req)
+
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+    if rc == 0:
+        status = 200
+    else:
+        status = 500
+
+    return JSONResponse(payload, status_code=status)
+
+
+def reply_processing(events: list[dict] | list[Any],
+                     extravars: dict[Any, Any],
+                     log_plain: str,
+                     rc,
+                     req: Request_ProxmoxVmsVMID_Clone) -> dict[str, list | Any]:
+
+    """ reply post-processing - json or ansible raw output """
+
+    if req.as_json:
+
+        ##### OUTPUT TYPE - as_json=True
+        #####
+
+        action = extravars["proxmox_vm_action"]
+        result = extract_action_results(events, action)
+
+        payload = {
+            "rc": rc,
+            "result": result
+            # "action": action,
+        }  # raw
+
+        # payload = {"rc": rc, "action": action, "result": events}
+    else:
+        ####
+        #### OUTPUT AS TEXT - as_json=False
+        ####
+
+        # payload = {"rc": rc, "log_plain": log_plain, "log_multiline": log_plain.splitlines()}
+        payload = {"rc": rc, "log_multiline": log_plain.splitlines()}
+    return payload
+
+
+def request_checks(req: Request_ProxmoxVmsVMID_Clone) -> dict[Any, Any]:
+    """ request checks """
+
+    extravars = {}
+
+    extravars["proxmox_vm_action"] = "vm_clone"
+    #
+    extravars["vm_name"] = "todo"  # add func to resolve vm_id to vm_name
+    if req.vm_id is not None:
+        extravars["vm_id"] = req.vm_id
+
+    if req.vm_new_id is not None:
+        extravars["vm_new_id"] = req.vm_new_id
+
+    if req.vm_name is not None:
+        extravars["vm_name"] = req.vm_name
+
+    if req.vm_description is not None:
+        extravars["vm_description"] = req.vm_description
+
+    if req.proxmox_node:
+        extravars["proxmox_node"] = req.proxmox_node
+
+    # nothing :
+    if not extravars:
+        extravars = None
+
+    if debug == 1:
+        print(f", extra vars : {extravars}")
+
+    extravars["hosts"] = "proxmox"
+    return extravars
+
---
+++ app/schemas/proxmox/vm_id/clone.py
@@ -0,0 +1,111 @@
+
+from typing import Literal
+from pydantic import BaseModel, Field
+
+#
+# ISSUE - #3
+#
+
+
+class Request_ProxmoxVmsVMID_Clone(BaseModel):
+
+    # '{"vm_id":100,
+    # "vm_new_id":1001,
+    # "vm_name":"test-cloned",
+    # "vm_description":"my description"}'
+
+    proxmox_node: str = Field(
+        ...,
+        # default= "px-testing",
+        description = "Proxmox node name",
+        pattern=r"^[A-Za-z0-9-]*$"
+    )
+
+    vm_id: str = Field(
+        ...,
+        # default="4000",
+        description="Virtual machine id",
+        pattern=r"^[0-9]+$"
+    )
+
+    vm_new_id: str = Field(
+        ...,
+        # default="5005",
+        description="New virtual machine id",
+        pattern=r"^[0-9]+$"
+    )
+
+    vm_description: str | None = Field(
+        default="cloned-vm",
+        description="Virtual machine meta description field",
+        pattern = r"^[A-Za-z0-9\s.,_\-]*$"
+    )
+
+    vm_name: str  = Field(
+        ...,
+        # default="new-vm",
+        description="Virtual machine meta name",
+        pattern = r"^[A-Za-z0-9-]*$"
+    )
+
+    as_json: bool = Field(
+        default=True,
+        description="If true : JSON output else : raw output"
+    )
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "proxmox_node": "px-testing",
+                "vm_id": "2000",
+                "vm_new_id": "3000",
+                "vm_name":"test-cloned",
+                "vm_description":"my description"
+            }
+        }
+    }
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+class Reply_ProxmoxVmsVMID_CloneItem(BaseModel):
+
+    action: Literal["vm_clone"]
+    source: Literal["proxmox"]
+    proxmox_node: str
+    vm_id: int = Field(..., ge=1)
+    vm_id_clone_from: int = Field(..., ge=1)
+    vm_name: str
+    vm_description: str
+    raw_info: str = Field(..., description="Raw string returned by proxmox")
+
+
+
+class Reply_ProxmoxVmsVMID_Clone(BaseModel):
+
+    rc: int = Field(0, description="RETURN code (0 = OK)")
+    result: list[Reply_ProxmoxVmsVMID_CloneItem]
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "rc": 0,
+                "result": [
+                    {
+                        "action": "vm_clone",
+                        "proxmox_node": "px-testing",
+                        "raw_info": {
+                            "data": "UPID:px-testing:0027CE9B:167F1A2C:68C03C17:qmclone:4004:API_master@pam!API_master:"},
+                        "source": "proxmox",
+                        "vm_description": "my description",
+                        "vm_id": "5004",
+                        "vm_id_clone_from": "4004",
+                        "vm_name": "test-cloned"
+                    }
+               ]
+            }
+        }
+    }
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
