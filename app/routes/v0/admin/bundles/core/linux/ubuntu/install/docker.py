f489f9d81acc38d36bbe05d932b8bb5c200009e0
Benjamin Collas <129123125+hyde-repo@users.noreply.github.com>
2025-09-18 18:54:49 +0200

    feat: (#281) - add route and request schema

---
+++ app/routes/v0/admin/bundles/core/linux/ubuntu/install/docker.py
@@ -0,0 +1,144 @@
+from typing import Any
+import logging
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
+
+from app.schemas.bundles.core.linux.ubuntu.install.docker import Request_BundlesCoreLinuxUbuntuInstall_Docker
+from app.schemas.bundles.core.linux.ubuntu.install.docker import Reply_BundlesCoreLinuxUbuntuInstall_Docker
+
+from app.runner import run_playbook_core  # , extract_action_results
+from app.json_extract import extract_action_results
+from app import utils
+from pathlib import Path
+import os
+
+#
+# ISSUE - #21
+#
+
+debug = 0
+
+router = APIRouter()
+logger = logging.getLogger(__name__)
+
+# PROJECT_ROOT = Path(__file__).resolve().parents[5]
+PROJECT_ROOT = Path(os.getenv("PROJECT_ROOT_DIR")).resolve()
+INVENTORY_NAME = "hosts"
+
+#
+# => /v0/admin/run/actions/core/linux/ubuntu/install/docker
+#
+@router.post(
+    path="/core/linux/ubuntu/install/docker",
+    summary="Install docker packages",
+    description="Install and configure docker engine on the target ubuntu system",
+    tags=["bundles - core - ubuntu "],
+    response_model=Reply_BundlesCoreLinuxUbuntuInstall_Docker,
+    # response_description="AAAAAAAAAAAAAAAAAA",
+)
+def bundles_core_linux_ubuntu_install_docker_router(req: Request_BundlesCoreLinuxUbuntuInstall_Docker):
+
+    action_name = "core/linux/ubuntu/install/docker"
+    #
+    checked_inventory_filepath = utils.resolve_inventory(INVENTORY_NAME)
+# checked_playbook_filepath  = utils.resolve_actions_playbook(action_name, "public_github")
+    checked_playbook_filepath = utils.resolve_bundles_playbook(action_name, "public_github")
+
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+    if debug == 1:
+        print("::  ACTION_NAME", action_name)
+        print("::  REQUEST ::", req.model_dump())
+        print(f":: PROJECT_ROOT  :: {PROJECT_ROOT} ")
+        print(f":: checked_inventory_filepath :: {checked_inventory_filepath} ")
+        print(f":: checked_playbook_filepath  :: {checked_playbook_filepath} ")
+
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+    extravars = request_checks(req)
+
+    ####
+
+    rc, events, log_plain, log_ansi = run_playbook_core(
+        checked_playbook_filepath,
+        checked_inventory_filepath,
+        limit=req.hosts,
+        extravars=extravars,
+    )
+
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+    payload = reply_processing(log_plain, rc)
+
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+    #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+    if rc == 0:
+        status = 200
+    else:
+        status = 500
+
+    return JSONResponse(payload, status_code=status)
+
+
+def reply_processing(log_plain: str,
+                     rc
+                     ) -> dict[str, list[str] | Any]:
+    """ reply post-processing - for ansible raw output """
+
+    payload = {"rc": rc, "log_multiline": log_plain.splitlines()}
+
+    return payload
+
+
+def request_checks(req: Request_BundlesCoreLinuxUbuntuInstall_Docker) -> dict[Any, Any]:
+    """ request checks """
+
+    extravars = {}
+
+    if req.proxmox_node:
+        extravars["proxmox_node"] = req.proxmox_node
+
+    if req.hosts is not None:
+        extravars["hosts"] = req.hosts
+
+    #
+
+    # if req.install_package_basics is not None:
+    #     extravars["INSTALL_PACKAGES_BASICS"] = req.install_package_basics
+    #
+    # if req.install_package_firewalls is not None:
+    #     extravars["INSTALL_PACKAGES_FIREWALLS"] = req.install_package_firewalls
+
+    if req.install_package_docker is not None:
+        extravars["INSTALL_PACKAGES_DOCKER"] = req.install_package_docker
+
+    # if req.install_package_docker_compose is not None:
+    #     extravars["INSTALL_PACKAGES_DOCKER_COMPOSE"] = req.install_package_docker_compose
+
+    # if req.install_package_utils_json is not None:
+    #     extravars["INSTALL_PACKAGES_UTILS_JSON"] = req.install_package_utils_json
+    #
+    # if req.install_package_utils_network is not None:
+    #     extravars["INSTALL_PACKAGES_UTILS_NETWORK"] = req.install_package_utils_network
+
+    if req.install_ntpclient_and_update_time is not None:
+        extravars["INSTALL_PACKAGES_NTP_AND_UPDATE_TIME"] = req.install_ntpclient_and_update_time
+
+    if req.packages_cleaning is not None:
+        extravars["SPECIFIC_PACKAGES_CLEANING"] = req.packages_cleaning
+
+    # nothing :
+    if not extravars:
+        extravars = None
+
+    if debug == 1:
+        print(f", extra vars : {extravars}")
+
+    # extravars["hosts"] = "proxmox"
+
+    return extravars
+
---
+++ app/schemas/bundles/core/linux/ubuntu/install/docker.py
@@ -0,0 +1,131 @@
+
+from typing import Literal
+from pydantic import BaseModel, Field
+
+#
+# ISSUE - #21
+#
+
+class Request_BundlesCoreLinuxUbuntuInstall_Docker(BaseModel):
+
+    proxmox_node: str = Field(
+        ...,
+        # default= "px-testing",
+        description = "Proxmox node name",
+        pattern=r"^[A-Za-z0-9-]*$"
+    )
+
+    hosts: str = Field(
+        ...,
+        description= "Hosts or groups",
+        pattern = r"^[a-zA-Z0-9._:-]+$"
+    )
+
+    # as_json: bool = Field(
+    #     default=True,
+    #     description="If true : JSON output else : raw output"
+    # )
+    #
+
+    ####
+    #
+    # install_package_basics : bool =  Field(
+    # ...,
+    # description="",
+    # )
+    #
+    # install_package_firewalls : bool =  Field(
+    # ...,
+    # description="",
+    # )
+
+    install_package_docker : bool =  Field(
+    ...,
+    description="",
+    )
+
+    # install_package_docker_compose: bool =  Field(
+    # ...,
+    # description="",
+    # )
+    #
+    # install_package_utils_json : bool =  Field(
+    # ...,
+    # description="",
+    # )
+    #
+    # install_package_utils_network : bool =  Field(
+    # ...,
+    # description="",
+    # )
+
+    ####
+
+    install_ntpclient_and_update_time: bool =  Field(
+    ...,
+        description="",
+    )
+
+    packages_cleaning: bool =  Field(
+    ...,
+        description="",
+    )
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "proxmox_node": "px-testing",
+                "hosts": "r42.vuln-box-00",
+                # "as_json": True,
+                #
+                # "install_package_basics" : True,
+                # "install_package_firewalls"  : False,
+                "install_package_docker"  : True,
+                # "install_package_docker_compose" : False,
+                # "install_package_utils_json"  : False,
+                # "install_package_utils_network" : False,
+                "install_ntpclient_and_update_time" : True,
+                "packages_cleaning" : True,
+
+            }
+        }
+    }
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+class Reply_BundlesCoreLinuxUbuntuInstall_DockerItem(BaseModel):
+
+    # action: Literal["vm_get_config"]
+    # source: Literal["proxmox"]
+    proxmox_node: str
+    # vm_id: int = Field(..., ge=1)
+    # vm_name: str
+    raw_data: str = Field(..., description="Raw string returned by proxmox")
+
+
+class Reply_BundlesCoreLinuxUbuntuInstall_Docker(BaseModel):
+
+    rc: int = Field(0, description="RETURN code (0 = OK)")
+    result: list[Reply_BundlesCoreLinuxUbuntuInstall_DockerItem]
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "rc": 0,
+                "result": [
+                    {
+                        # "action": "vm_get_config",
+                        # "proxmox_node": "px-testing",
+                        # ...
+                        # "source": "proxmox",
+                        # "vm_id": "1000"
+                    }
+                ]
+            }
+        }
+    }
+
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#### #### #### #### #### #### #### #### #### #### #### #### #### ####
